97f102160a9ea1b6cd71d86ba55a9096738ca7fe
gmcn-work <63479322+gmcn-work@users.noreply.github.com>
2023-01-28 16:56:02 -0600

    Update telephone-number-validator.js

--- telephone-number-validator.js
+++ telephone-number-validator.js
@@ -1,11 +1,6 @@
 function telephoneCheck(str) {
-  var re = /(^1*\s*\(*\d{3}\s*\)*)(-*\s*\d{3}\s*-*)(\d{4})$/g;
-  var par = /\((?!...\))|(?<!\(...)\)/g;
-  if(par.test(str)) 
-  {
-    return false;
-  }
-  return re.test(str);
+  let regex = /^[1]{0,1}[\s]*(?:\(\d{3}\)|\d{3})[\s-]*\d{3}[\s-]*\d{4}$/gm;
+  return regex.test(str);
 }
 
 telephoneCheck("555-555-5555");
